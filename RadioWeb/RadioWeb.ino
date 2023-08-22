9e13a409f688a3bbeb52d0a94e2a2775b14c4669
renatoianhez <34423009+renatoianhez@users.noreply.github.com>
2023-08-21 23:34:52 -0300

    Option to place radios in a Gist
    In this mode, you can update the radios through a text file hosted in Gist.

--- RadioWeb/RadioWeb.ino
+++ RadioWeb/RadioWeb.ino
@@ -1,19 +1,19 @@
 #include <Arduino.h>
-#include <TFT_eSPI.h> // Biblioteca da tela
+#include <TFT_eSPI.h> // Screen lib
 #include <SPI.h> // SPI
-#include <WiFi.h> // Pra funcionar o WiFi
-#include <WiFiMulti.h> // Pra mais de um local de WiFi
-#include "Audio.h" // Biblioteca do audio
-#include "examples/320 x 240/Free_Font_Demo/Free_Fonts.h" // Fontes
-#include "BluetoothA2DPSink.h" // Para o Bluetooth
-#include <HTTPClient.h>
+#include <WiFi.h> // WiFi lib
+#include <WiFiMulti.h> // Multi WiFi
+#include "Audio.h" // Audio lib
+#include "examples/320 x 240/Free_Font_Demo/Free_Fonts.h" // Fonts
+#include "BluetoothA2DPSink.h" // Bluetooth lib
+#include <HTTPClient.h> // HTTP lib
 
-#define canalPraCima  0   // Botoes para mudar a estacao
+#define canalPraCima  0   // Buttons to change stations
 #define canalPraBaixo 4
-#define I2S_DOUT      25  // Pinos do audio I2S
+#define I2S_DOUT      25  // I2S audio pins
 #define I2S_BCLK      27
 #define I2S_LRCK      26
-#define modoAparelho  34  // Chave seletora radio-bluetooth
+#define modoAparelho  34  // Radio-bluetooth switch
 
 bool mudouEstacao;
 int radioTocando;
@@ -21,7 +21,7 @@ char* tituloBT;
 char* artistaBT;
 char* albumBT;
 
-TFT_eSPI tela = TFT_eSPI(); // Os pinos da tela sao definidos na biblioteca
+TFT_eSPI tela = TFT_eSPI(); // Screen pins is in lib
 Audio audio;
 BluetoothA2DPSink a2dp_sink;
 WiFiMulti outrosWiFi;
@@ -35,7 +35,14 @@ String nomeRadio[30];
 String tipoRadio[30];
 String cidadeRadio[30];
 String urlRadio[30];
-String endTudo = "https://gist.githubusercontent.com/renatoianhez/4b5726d5ef32ad0a24fa3db6521989d6/raw";
+String endTudo = "https://gist.githubusercontent.com/XXX/XXX/raw"; // Place here the radio station data address
+      // The data format is:
+      //   Name of Station (max 14 caracters) ex. KFMG Radio
+      //   City-State-Country of the station ex. London UK
+      //   Type of station (Web or FM) and genre ex. Web - Rock/Alternative
+      //   Station's stream URL ex. https://antenaone.crossradio.com.br/stream/1;
+      //  Attention: you must enter the data of 30 radio stations!
+
 int indice = 0;
 
 void baixaDados(const String& urlDado, int maxTokens, String* tokens) {
@@ -43,10 +50,10 @@ void baixaDados(const String& urlDado, int maxTokens, String* tokens) {
   int httpCode = http.GET();
   if (httpCode == HTTP_CODE_OK) {
     conteudoBaixado = http.getString();
-    Serial.println("Arquivo de dados das radios baixado com sucesso");
+    Serial.println("Data file dowloaded!");
   }
   else {
-    Serial.printf("Falha ao baixar o arquivo. Código de erro HTTP: %d\n", httpCode);
+    Serial.printf("Error in file download. HTTP error code: %d\n", httpCode);
   }
   http.end();
   int startIndex = 0;
@@ -66,26 +73,26 @@ void baixaDados(const String& urlDado, int maxTokens, String* tokens) {
 
 void sobeCanal() {
   // Se apertar o botao pra subir uma estacao:
-  radioTocando = radioTocando + 1;          // incrementa o indice
+  radioTocando = radioTocando + 1;          // increments the index
   tela.fillScreen(TFT_BLACK);
   tela.setTextColor(TFT_RED, TFT_WHITE);
   tela.setCursor(0, 0, 4);
   tela.println("Sintonizando!");
   delay(1000); // uma pausa pra largar o botao
-  if (radioTocando > 29) {     // Testa se passou da ultima estacao da lista
-    radioTocando = 0;         // se sim vai pra primeira da playlist
+  if (radioTocando > 29) { 
+    radioTocando = 0; 
   }
 }
 
 void desceCanal() {
-  radioTocando = radioTocando - 1;        // decrementa o indice
+  radioTocando = radioTocando - 1;        // decrements the index
   tela.fillScreen(TFT_BLACK);
   tela.setCursor(0, 0, 4);
   tela.setTextColor(TFT_RED, TFT_WHITE);
   tela.println("Sintonizando!");
   delay(1000); // uma pausa pra largar o botao
-  if (radioTocando < 0) {     // Testa se passou pra baixo da primeira da lista
-    radioTocando = 29;      // se sim vai pra ultima da playlist
+  if (radioTocando < 0) {    
+    radioTocando = 29;     
   }
 }
 
@@ -96,7 +103,7 @@ void mudouEstacaoMesmo() {
   int centro = 77 - (11 * ((nomeRadio[radioTocando].length())) / 2);
   tela.setCursor(centro, 18);
   tela.setTextColor(TFT_BLUE); //texto azul
-  tela.println(nomeRadio[radioTocando]); // nome da radio centralizado
+  tela.println(nomeRadio[radioTocando]); 
   tela.setTextFont(2);
   tela.setCursor(0, 30);
   tela.setTextColor(TFT_ORANGE);
@@ -112,12 +119,12 @@ void audio_info(const char *info) {
   Serial.println(info);
 }
 
-void audio_showstreamtitle(const char *titulo) { // Pra mostrar o nome da musica e cantor
+void audio_showstreamtitle(const char *titulo) { // Display tags
   tela.setCursor(0, 60, 2);
   tela.setTextColor(TFT_CYAN, TFT_BLACK);
   tela.println("Tocando agora:");
   tela.setCursor(0, 75, 2);
-  tela.println("                                                                                    "); //Pra limpar
+  tela.println("                                                                                    "); //To clean
   tela.setCursor(0, 75, 2);
   tela.println(titulo);
 }
@@ -128,7 +135,7 @@ void avrc_metadata_callback(uint8_t id, const uint8_t *text) {
   tela.fillScreen(TFT_BLACK);
   tela.fillRect(0, 0, 160, 28, TFT_WHITE);
   tela.setCursor(28, 18);
-  tela.setTextColor(TFT_BLUE); //texto azul
+  tela.setTextColor(TFT_BLUE);
   tela.println("BLUETOOTH");
   tela.setTextFont(2);
   tela.setCursor(0, 30);
@@ -152,32 +159,32 @@ void avrc_metadata_callback(uint8_t id, const uint8_t *text) {
 }
 
 void setup() {
-  pinMode(canalPraCima, INPUT_PULLUP); // Modo dos botoes
+  pinMode(canalPraCima, INPUT_PULLUP); 
   pinMode(canalPraBaixo, INPUT_PULLUP);
   pinMode(modoAparelho, INPUT);
-  Serial.begin( 115200 ); // inicia o serial
-  tela.init(); // Inicia o display
-  tela.setRotation(1); // Modo paisagem
-  tela.invertDisplay(0); // Necessario para acertar as cores
-  outrosWiFi.addAP("Renato", "abacatequecaiudope"); // Dados do WiFi local
+  Serial.begin( 115200 ); 
+  tela.init(); 
+  tela.setRotation(1); // Landscape mode
+  tela.invertDisplay(0); // Color mode adjust. If the colors are wrong, use 1
+  outrosWiFi.addAP("XXX", "XXX"); // WiFi data (user, password)
   //outrosWiFi.addAP("xxx", "xxx");
   //outrosWiFi.addAP("xxx","xxx");
 
   if (digitalRead(modoAparelho)) {
-    radioTocando = random(0, 30); // Sorteia e primeira estacao
+    radioTocando = random(0, 30); // The first station is randomized
     mudouEstacao = true;
-    WiFi.disconnect(); // Confere se o WiFi ta desligado
+    WiFi.disconnect(); 
     WiFi.mode(WIFI_STA);
-    while (outrosWiFi.run() != WL_CONNECTED) delay(1500); // Tenta conectar nas redes
-    audio.setPinout(I2S_BCLK, I2S_LRCK, I2S_DOUT); // Configura o audio
+    while (outrosWiFi.run() != WL_CONNECTED) delay(1500); 
+    audio.setPinout(I2S_BCLK, I2S_LRCK, I2S_DOUT); 
     audio.setVolume(16); // volume vai de zero a 21
-    audio.setConnectionTimeout(2000, 7200); // Garante a conexao as estacoes
-    audio.setTone(4, 0, 4); // Equalizador grave - medio - agudo (-40 a 6)
+    audio.setConnectionTimeout(2000, 7200); 
+    audio.setTone(4, 0, 4); // Equalizator bass - mid - terble (-40 to 6)
     // audio.setBufsize(0, 4000000);
-    // audio.forceMono(true); // Força o mono
-    // audio.setBalance(0); // Balanco estereo direito - esquerdo (-16 a 16)
-    baixaDados(endTudo, 120, tudo); // Baixa dados das radios
-    for (int j = 0; j < 120; j = j + 4) { // Separa os dados
+    // audio.forceMono(true); // Mono option
+    // audio.setBalance(0); // Balance right - left (-16 to 16)
+    baixaDados(endTudo, 120, tudo); // Download radio data
+    for (int j = 0; j < 120; j = j + 4) { // Separates data into its types
       nomeRadio[indice] = tudo[j];
       tipoRadio[indice] = tudo[(j + 1)];
       cidadeRadio[indice] = tudo[(j + 2)];
@@ -185,7 +192,7 @@ void setup() {
       indice++;
     }
   }
-  else {
+  else {                               // Bluetooth option
     i2s_pin_config_t my_pin_config = {
       .bck_io_num = 27,
       .ws_io_num = 26,
@@ -202,17 +209,17 @@ void setup() {
 void loop() {
   if (digitalRead(modoAparelho)) {
     audio.loop();
-    if (digitalRead(canalPraCima) == LOW) {//Se apertar o botao pra subir a estacao
+    if (digitalRead(canalPraCima) == LOW) {//Up station
       sobeCanal();
-      mudouEstacao = true;   // Avisa la que mudou de estacao!
+      mudouEstacao = true; 
     }
-    if (digitalRead(canalPraBaixo) == LOW) {//Se apertar o botao para descer a estacao:
+    if (digitalRead(canalPraBaixo) == LOW) {//Down station
       desceCanal();
-      mudouEstacao = true;  //Avisa la que mudou de estacao tambem!
+      mudouEstacao = true;  
     }
-    if (mudouEstacao) {     //Se a estacao mudou, atualiza o LCD e o terminal
+    if (mudouEstacao) {     //If change station, update the screen
       mudouEstacaoMesmo();
-      mudouEstacao = false; // Mantem a estacao que esta tocando
+      mudouEstacao = false;
     }
   }
 }
